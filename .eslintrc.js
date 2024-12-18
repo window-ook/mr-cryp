f4299d496e4d4378e473e331fa40d117f65448a7
devkoow <cwl64658@gmail.com>
2024-12-18 23:08:10 +0900

    [Code] eslintrc.js, READ.ME

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'no-undef': 'error',
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+      },
+    ],
+  },
+};
