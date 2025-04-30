27cb4226bc4582e5f8d98b99ac8c9883937e7eda
devkoow <cwl64658@gmail.com>
2025-04-30 23:43:00 +0900

    [Fix] 시황 마키 수정

--- components/trends/market-situation/MarketSituation.jsx
+++ components/trends/market-situation/MarketSituation.jsx
@@ -1,17 +1,17 @@
-import { memo, useEffect, useRef, useState } from 'react';
+import { memo, useEffect, useState } from 'react';
 import { useMarketSituationQuery } from '@/hooks/useMarketSituationQuery';
 import { LinearProgress } from '@mui/material';
+import { AnimatePresence } from 'motion/react';
+import * as m from 'motion/react-m';
 
 function MarketSituation() {
-  const [index, setIndex] = useState(0);
-
-  const containerRef = useRef(null);
-  const timeoutRef = useRef(null);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const formatDate = pubDate => {
     const date = new Date(pubDate);
     return `${date.getFullYear()}년 ${date.getMonth() + 1}월 ${date.getDate()}일`;
   };
+
   const today = formatDate(new Date());
 
   const { data: situations, isPending } = useMarketSituationQuery();
@@ -19,56 +19,43 @@ function MarketSituation() {
   useEffect(() => {
     if (!situations || situations.length === 0) return;
 
-    const startTimer = () => {
-      timeoutRef.current = setTimeout(() => {
-        setIndex(prevIndex => (prevIndex + 1) % situations.length);
-      }, 5000);
-    };
+    const interval = setInterval(() => {
+      setCurrentIndex(prevIndex => (prevIndex + 1) % situations.length);
+    }, 4000);
 
-    const handleAnimationEnd = () => {
-      clearTimeout(timeoutRef.current);
-      startTimer();
-    };
-
-    const container = containerRef.current;
-    if (container) {
-      container.addEventListener('animationend', handleAnimationEnd);
-    }
-
-    startTimer();
-
-    return () => {
-      clearTimeout(timeoutRef.current);
-      if (container) {
-        container.removeEventListener('animationend', handleAnimationEnd);
-      }
-    };
-  }, [situations, index]);
+    return () => clearInterval(interval);
+  }, [situations, currentIndex]);
 
   if (isPending) return <LinearProgress />;
 
-  const currentNews = situations?.[index];
+  const currentNews = situations?.[currentIndex];
 
   return (
     <div className="flex flex-col gap-4">
       <header className="flex items-end gap-2">
-        <span className="font-pretendard text-2xl max-[475px]:text-xl font-bold text-main-dark">
+        <span className="font-pretendard text-2xl max-[475px]:text-xl font-bold">
           시황
         </span>
         <span className="text-gray-500 font-ng text-xs">{today}</span>
       </header>
       <article className="relative h-10 p-2 rounded-lg flex items-center">
-        <button
-          aria-label="기사 링크로 이동하기"
-          type="button"
-          ref={containerRef}
-          className="absolute overflow-hidden whitespace-nowrap text-ellipsis inset-0 flex items-center animate-marqueeY cursor-pointer"
-          onClick={() => window.open(currentNews?.url, '_blank')}
-        >
-          <span className="font-semibold max-[1100px]:text-xs">
-            {currentNews?.title}
-          </span>
-        </button>
+        <AnimatePresence mode="popLayout">
+          <m.button
+            key={currentIndex}
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ y: -20, opacity: 0 }}
+            transition={{ duration: 0.5 }}
+            aria-label="기사 링크로 이동하기"
+            type="button"
+            className="absolute overflow-hidden whitespace-nowrap text-ellipsis inset-0 flex items-center cursor-pointer"
+            onClick={() => window.open(currentNews?.url, '_blank')}
+          >
+            <span className="font-semibold text-sm sm:text-base">
+              {currentNews?.title}
+            </span>
+          </m.button>
+        </AnimatePresence>
       </article>
     </div>
   );
--- components/trends/videos/Videos.jsx
+++ components/trends/videos/Videos.jsx
@@ -7,11 +7,12 @@ import VideosUI from './VideosUI';
 function Videos() {
   const theme = useTheme();
 
-  const { data: videos, isPending, error } = useVideosQuery('코인 추천');
+  // const { data: videos, isPending, error } = useVideosQuery('코인 추천');
 
-  if (isPending) return <PendingUI />;
+  // if (isPending) return <PendingUI />;
 
-  if (error) throw new Error('유튜브 영상 다운로드 중 에러가 발생했습니다.');
+  // if (error) throw new Error('유튜브 영상 다운로드 중 에러가 발생했습니다.');
+  const videos = [];
 
   return <VideosUI videos={videos} theme={theme} />;
 }
